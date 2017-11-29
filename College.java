07ac971ba3084d1c7a875a19d2e168565d242839
sarahpadlipsky <Sarah.Padlipsky@gmail.com>
2017-11-28 20:37:21 -0800

    Add a College class and the reading in of the data

---
+++ College.java
@@ -0,0 +1,55 @@
+
+public class College {
+  // Name of the college.
+	private String name;
+	// Median salary after 10 years.
+	private int medianSalary;
+	// Average SAT for admitted freshman.
+	private int avgSAT;
+ 	// Average GPA for admitted freshman.
+	private double avgGPA;
+	// Acceptance Rate at the college.
+	private double accRate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMedianSalary() {
+		return medianSalary;
+	}
+
+	public void setMedianSalary(int medianSalary) {
+		this.medianSalary = medianSalary;
+	}
+
+	public int getAvgSAT() {
+		return avgSAT;
+	}
+
+	public void setAvgSAT(int avgSAT) {
+		this.avgSAT = avgSAT;
+	}
+	
+	public double getAvgGPA() {
+		return avgGPA;
+	}
+
+	public void setAvgGPA(double avgGPA) {
+		this.avgGPA = avgGPA;
+	}
+
+	public double getAccRate() {
+		return accRate;
+	}
+
+	public void setAccRate(double accRate) {
+		this.accRate = accRate;
+	}
+}
+
+
